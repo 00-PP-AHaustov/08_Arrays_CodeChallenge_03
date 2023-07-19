441d605d0c171ce68d368964a3bb2630bb755c7b
Anton <anton.haust@gmail.com>
2023-07-19 12:41:20 +0200

    challenge fertig

--- script_01.js
+++ script_01.js
@@ -1,4 +1,3 @@
-
 /*** Mini-Challenge  */
 // Satzbau + Punktuation
 
@@ -8,26 +7,63 @@
 // "E" (exclamation) --> "!"  Ich bin!
 
 // Parameter --> funktion getSentence([ ...], "Q"){ ...}
-
-
+const GAP = " ";
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-output(getSentence(["Ich","bin","Max"],"S"));
-output(getSentence(["Bist","du","Max"],"Q"));
-output(getSentence(["Ich","bin"],"E"));
-output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+output(getSentence(["Ich", "bin", "Max"], "S"));
+output(getSentence(["Bist", "du", "Max"], "Q"));
+output(getSentence(["Ich", "bin"], "E"));
+output(
+  getSentence(
+    [
+      "Ich",
+      "hätt",
+      "gern",
+      "die",
+      "Platt",
+      "von",
+      "dene",
+      "zwei",
+      "diwodaso",
+      "Spass",
+      "mache,",
+      "habbe",
+      "Sie",
+      "die",
+    ],
+    "Q"
+  )
+);
 
-function getSentence(arr,op) {
-   
-   
-   
-     return "toDo";
+function getSentence(arr, op) {
+  switch (op) {
+    case "Q":
+      arr.push("?");
+      break;
+    case "S":
+      arr.push(".");
+      break;
+    case "E":
+      arr.push("!");
+      break;
+    default:
+      break;
+  }
+  return buildSentence(arr);
 }
 
-
+function buildSentence(arr) {
+  let result = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (i != arr.length - 1) {
+      result += GAP + arr[i];
+    } else result += arr[i];
+  }
+  return result.trim();
+}
 
 // Modul: Ausgabe in Konsole : Test
-// output("hi"); 
+// output("hi");
 function output(outputData) {
-    console.log(outputData);
+  console.log(outputData);
 }
